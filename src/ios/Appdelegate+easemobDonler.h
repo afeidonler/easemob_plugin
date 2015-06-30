896c1b4dfe96fcf0adbe6b2e32bd052486ff8de7
Maggie <maggie03230@gmail.com>
2015-06-30 11:15:21 +0800

    iOS bug 修正,增加会话列表排序、发送消息返回回调

--- src/ios/Appdelegate+easemobDonler.h
+++ src/ios/Appdelegate+easemobDonler.h
@@ -1,6 +1,7 @@
 #import "AppDelegate.h"
 
-@interface AppDelegate (notification)
+@interface AppDelegate (Easemob)
 - (void)application:(UIApplication *)application didRegisterForRemoteNotificationsWithDeviceToken:(NSData *)deviceToken;
 - (void)application:(UIApplication *)application didFailToRegisterForRemoteNotificationsWithError:(NSError *)error;
 - (void)application:(UIApplication *)application didReceiveRemoteNotification:(NSDictionary *)userInfo;
+@end
